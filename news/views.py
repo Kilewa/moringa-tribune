26c8de0306613889d804a00b52298731837a8c05
kilewa <kilewageorge230@gmail.com>
2020-10-07 13:37:48 +0300

    created db models

--- news/views.py
+++ news/views.py
@@ -20,14 +20,7 @@ def news_of_day(request):
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html) 
+    return render (request,'all-news/today-news.html', {"date": date})
 
 
 def convert_dates(dates):
